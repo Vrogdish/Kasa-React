b4464db237c1c032faa44e1c8101177bae8fb13b
Vrogdish <vrogdish@outlook.com>
2023-06-23 13:29:32 +0200

    modif readme + build

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,18 +1,20 @@
-import { Link } from "react-router-dom"
-import "./Error.css"
+import { Link } from 'react-router-dom'
+import './Error.css'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
-
-function ErrorPage () {
-
-
-
-    return (
-        <div className="error-page">
-            <h1>404</h1>
-            <h2>Oups! La page que vous demandez n'existe pas.</h2>
-            <Link to="/"> Retourner sur la page d’accueil </Link>
-        </div>
-    )
+function ErrorPage() {
+  return (
+    <>
+      <Header />
+      <div className="error-page">
+        <h1>404</h1>
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        <Link to="/"> Retourner sur la page d’accueil </Link>
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default ErrorPage
